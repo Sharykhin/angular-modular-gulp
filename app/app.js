443d583a7bed20582b33a6462bbf28dc69e374e1
chapal <chapal@inbox.ru>
2015-04-07 00:19:44 +0300

    add copy task, for copying templates

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
 'use strict';
-         
+            
 var App = angular.module('App',['User','ui.router']);
 
--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,40 @@ var concat = require('gulp-concat');
 var gls = require('gulp-live-server');
 var watch = require('gulp-watch');
 var jsmin = require('gulp-jsmin');
-var uglyfly = require('gulp-uglyfly')
+var uglyfly = require('gulp-uglyfly');
+var filelog = require('gulp-filelog');
+var copy = require( 'gulp-contrib-copy' );
 
-gulp.task('serve',['scripts.vendor','watch'],function(){
+gulp.task('serve',['scripts.vendor','scripts.app','templates','watch'],function(){
 	var server = gls.static('public');
 	server.start();
 	gulp.watch(['./app/**/*.js'],server.start);
 });
 
 gulp.task('watch',function(){
-	watch('app/**/*.js',function(){
+	watch('app/**/*.js',function(){		
 		gulp.start('scripts.app');
+		gulp.start('templates');
 	});
+});
+
+gulp.task('log',function(){
+	gulp.src('app/**/*.js')
+		.pipe(filelog())
+		.pipe(gulp.dest('./logs/'))
 });
 
 gulp.task('scripts.app',function(){
 	return gulp.src('./app/**/*.js')
 		.pipe(concat('app.js'))
-		.pipe(jsmin())
-		.pipe(uglyfly())		
+		.pipe(jsmin())		
 		.pipe(gulp.dest('./public/'));
 });
 
 gulp.task('scripts.vendor',function(){
 	var source = [
 		'public/vendor/angular/angular.js',		
-		'public/vendor/angular-ui-router/release/angular-ui-router.min.js'
+		'public/vendor/angular-ui-router/release/angular-ui-router.js'
 	];
 
 	return gulp.src(source)
@@ -36,4 +44,10 @@ gulp.task('scripts.vendor',function(){
 		.pipe(gulp.dest('./public/assets/js/'))
 });
 
+
+gulp.task('templates',function(){
+	gulp.src('./app/views/*.html')
+		.pipe(copy({log:true}))
+		.pipe(gulp.dest('./public/views/'))
+});
 
