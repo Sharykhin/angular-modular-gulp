af98e0cd3176e96c5548d0ef16e6721b351a9008
chapal <chapal@inbox.ru>
2015-04-06 23:53:07 +0300

    ISSUE-#1 has been completed. JSmin and uglyfly tasks have been added

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ var concat = require('gulp-concat');
 var gls = require('gulp-live-server');
 var watch = require('gulp-watch');
 var jsmin = require('gulp-jsmin');
+var uglyfly = require('gulp-uglyfly')
 
-gulp.task('serve',['watch'],function(){
+gulp.task('serve',['scripts.vendor','watch'],function(){
 	var server = gls.static('public');
 	server.start();
 	gulp.watch(['./app/**/*.js'],server.start);
@@ -19,8 +20,20 @@ gulp.task('watch',function(){
 gulp.task('scripts.app',function(){
 	return gulp.src('./app/**/*.js')
 		.pipe(concat('app.js'))
-		.pipe(jsmin())		
+		.pipe(jsmin())
+		.pipe(uglyfly())		
 		.pipe(gulp.dest('./public/'));
+});
+
+gulp.task('scripts.vendor',function(){
+	var source = [
+		'public/vendor/angular/angular.js',		
+		'public/vendor/angular-ui-router/release/angular-ui-router.min.js'
+	];
+
+	return gulp.src(source)
+		.pipe(concat('vendor.js'))
+		.pipe(gulp.dest('./public/assets/js/'))
 });
 
 
